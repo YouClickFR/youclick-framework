f00ee04ad84abe8d73f36ea08fb8392aab591194
Paul Leflon <leflon.paul@gmail.com>
2018-12-10 09:15:51 +0100

    clarified code

--- assets/js/interviewEvents.js
+++ assets/js/interviewEvents.js
@@ -1,1 +1,4 @@
-$(document).keydown((e) => { if (e.keyCode == 27) $(".interviewContainer.active").removeClass("active")});
+$(document).keydown((e) => { 
+  if (e.keyCode == 27) 
+    $(".interviewContainer.active").removeClass("active");
+});
