e0dc39fb46eae2d8d7971d64763ef0ccaf9d2411
Paul Leflon <leflon.paul@gmail.com>
2018-12-10 09:17:17 +0100

    Removed useless console.log

--- assets/js/youclickHeaderParallax.js
+++ assets/js/youclickHeaderParallax.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-            console.log("here");
 		$("#youclickHeader").scroll(() => {
 			$("#youclickHeader").css("background-position","center -" + $(document).scrollTop()/1.3 + "px");
         });
